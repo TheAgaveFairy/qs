4f6cc0d774aa020f932d3e50789f80fba6193f4a
TheAgaveFairy <93168751+TheAgaveFairy@users.noreply.github.com>
2024-10-19 17:53:01 -0500

    cutoff change

--- makefile
+++ makefile
@@ -5,13 +5,13 @@ CPFLAGS = -g -pthread
 all: quicksort quicksort_p
 
 quicksort: quicksort.o helpers.o
-	$(CC) $(CFLAGS) -o $@ $^
+	$(CC) $(CFLAGS) -o $@ $^ -lm
 
 quicksort_p: quicksort_p.o helpers.o
 	$(CC) $(CPFLAGS) $(CFLAGS)  -o $@ $^
 
 quicksort.o: quicksort.c quicksort.h helpers.h
-	$(CC) $(CFLAGS) -c $<
+	$(CC) $(CFLAGS) -c $< -lm
 
 quicksort_p.o: quicksort_p.c quicksort_p.h helpers.h
 	$(CC) $(CFLAGS) $(CFLAGS) -c $<
--- quicksort.c
+++ quicksort.c
@@ -8,32 +8,30 @@
 #include "quicksort.h"
 #include "helpers.h"
 
+/*------------------DEPRECATED
 #define ARRAY_SIZE (INT_MAX / 2) //(INT_MAX / 2) smaller for testing
-#define DEBUG 0 //if 1, will print human readable statements to stdout. if 0, outputs for redirection that will come to .csv
 #define CUTOFF 1000 //Claude suggested parameter - probably going to dynamically calculate instead
+*/
 
-int d_max = 0;
-int cutoff = 0;
+#define DEBUG 0 //if 1, will print human readable statements to stdout. if 0, outputs for redirection that will come to .csv
+
+int d_max = 0;  // counts how deep we are in recursion
+int cutoff = 0; // how deep we'll allow the recursion to go
 int array_size;
+
 int main(int argc, char * argv[]){
     if(argc != 3){
         fprintf(stderr, "Please give the number of threads and array size. ex. for 12 threads and len 2^13./quicksort 12 8192");
         return EXIT_FAILURE;
     }
-    /*
-    if(DEBUG){
-        printf("Array Size: %d\n", ARRAY_SIZE);
-    } */   
-    
-    double temp = log(ARRAY_SIZE) / log(2);
-    cutoff = (int)temp;
-    //cutoff = 10000;
-    if(DEBUG) printf("Cutoff log_2(ARRAY_SIZE): %d\n", cutoff);    
-
-    int num_threads = atoi(argv[1]);
+   
+    int num_threads = atoi(argv[1]); //not super safe but
     array_size = atoi(argv[2]);
-   	
-    //int num_threads = getNumThreads(DEBUG);
+    
+    double temp = log(array_size) / log(2);
+    cutoff = (int)temp;
+    if(DEBUG) printf("Cutoff log_2(ARRAY_SIZE): %d\n", cutoff);    
+	
 	omp_set_num_threads(num_threads);
 
 	double start_time = omp_get_wtime();
@@ -42,12 +40,9 @@ int main(int argc, char * argv[]){
 		fprintf(stderr, "Allocation failed. Exiting!");
 		return EXIT_FAILURE;
 	}
+
     double qs_begin_time = omp_get_wtime();
-
-    //printArray(test_array, ARRAY_SIZE);
     quicksort(test_array, array_size);
-    //printArray(test_array, ARRAY_SIZE);
-
 	double end_time = omp_get_wtime();
     
     if(DEBUG){
@@ -55,7 +50,6 @@ int main(int argc, char * argv[]){
 	    printf("Max. Depth: %d\n", d_max);
         printf("Time for arraygen:\t%lf\nTime for quicksort:\t%lfs\nTotal time:\t\t%lf\n", qs_begin_time-start_time, end_time-qs_begin_time, end_time-start_time);
 	} else {
-        //printf("%d, %d, %lf, %lf, %lf\n", num_threads, ARRAY_SIZE, qs_begin_time-start_time, end_time-qs_begin_time, end_time-start_time);
         printf("%d, %d, %lf\n", num_threads, array_size, end_time-qs_begin_time);
     }
 	free(test_array);
@@ -81,7 +75,7 @@ void quicksort(int array[], int length){
 void quicksort_recursion(int array[], int low, int high, int depth){
     d_max = depth > d_max? depth : d_max;
     if(low < high){
-        if(depth > CUTOFF){ //do sequential
+        if(depth > cutoff){ //do sequential
             insertion_sort(array, 0, array_size);
 		    //int pivot_index = partition(array, low, high);
 		    //quicksort_recursion(array, low, pivot_index - 1, depth+1);
